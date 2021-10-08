44458c0022cf4ecfbf73ba447190cd6b7a379f1c
Albert SHilajyan <alikshilajyan@mail.ru>
2021-10-08 17:47:59 +0400

    homework2

--- src/homework2.java
+++ src/homework2.java
@@ -1,15 +1,25 @@
 public class homework2 {
     public static void main(String[] args) {
         for (int i = 0; i < 4; i++) {
-            for (int j = 4; j < 5; j++) {
-                for (int k = 0; k <= i; k++) {
-                    System.out.print(" *");
+            for (int j = i; j < 4; j++){
 
-                }
-                System.out.println();
-
+                System.out.print("* ");
             }
 
+            System.out.println();
+
         }
+
+
     }
 }
+
+
+
+
+
+
+
+
+
+
