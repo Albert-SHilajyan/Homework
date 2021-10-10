e7ed1e0467ce964a2f4198b6499f687d181ac3da
Albert SHilajyan <alikshilajyan@mail.ru>
2021-10-11 00:20:57 +0400

    homework3

--- src/homework3.java
+++ src/homework3.java
@@ -1,17 +1,18 @@
 public class homework3 {
     public static void main(String[] args) {
         for (int i = 0; i < 4; i++) {
-
-            for (int j = i; j < 3; j++) {
-                System.out.print(" *");
-
-                }System.out.println(" *");
-
-
-
+            for (int j = 4; j >i; j--) {
+                System.out.print("  ");
             }
-
+            for (int j = 0; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
 
         }
     }
+
+}
+
+
 
