c29dff45a2ff16c28397df665820b16daa4fd698
Albert SHilajyan <alikshilajyan@mail.ru>
2021-10-18 18:54:28 +0400

    ArrayUtil

---
+++ src/arrayutil/ArrayUtil.java
@@ -0,0 +1,76 @@
+package arrayutil;
+
+public class ArrayUtil {
+    public static void main(String[] args) {
+        int[] array = {2, 5, 8, 10, 32, 22, 11, 66, 12, 30};
+        for (int i = 0; i < array.length; i++) {
+
+            System.out.print(array[i] + " ");
+        }
+        System.out.println();
+        int max = array[0];
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] > max) {
+                max = array[i];
+            }
+
+        }
+        System.out.println("max =" + max);
+
+
+        int min = array[0];
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] < min) {
+                min = array[i];
+            }
+
+        }
+        System.out.println("min =" + min);
+
+        System.out.println("print all evens");
+
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] % 2 == 0) {
+                System.out.print(array[i] + " ");
+            }
+
+        }
+        System.out.println();
+        System.out.println("print all odds");
+
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] % 2 == 1) {
+                System.out.print(array[i] + " ");
+            }
+
+        }
+        System.out.println();
+
+        int evenCount = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] % 2 == 0) {
+                evenCount++;
+
+
+            }
+        }
+        System.out.print("evenCount =" + " " + evenCount);
+
+        System.out.println();
+        int oddCount = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] % 2 == 1) {
+                oddCount++;
+            }
+
+        }
+        System.out.println("oddCount = " + oddCount);
+        int sum = 0;
+        for (int i = 0; i < array.length; i++) {
+            sum += array[i];
+
+        }
+        System.out.println("sum = " + sum);
+        System.out.println("avg: " + sum / array.length);
+    }
+}
